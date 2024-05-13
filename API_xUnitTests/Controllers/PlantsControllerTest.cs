9c638102c9645bbba59e901707db95a03c6796db
Sokolice <jana.sokolovska@outlook.com>
2024-05-13 08:32:06 +0200

    added project for unit testing

---
+++ API_xUnitTests/Controllers/PlantsControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using API.Controllers;
+using API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_xUnitTests.Controllers
+{
+    public class PlantsControllerTest
+    {
+        PlantsController _controller;
+        IPlantsService _service;
+        public PlantsControllerTest()
+        {
+            _controller = new PlantsController(_service);
+        }
+
+        [Fact]
+        public void GetAllPlantsTest()
+        {
+            var result = _controller.GetPlants();
+        }
+    }
+}
